9df9344db6bdad994d9c3784b4fb91471c7c5c27
anggaaprianda04 <anggaaprianda04@gmail.com>
2023-11-10 00:23:20 +0700

    refactor reusable components

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,16 @@
-import CardAnimeList from "@/components/CardAnimeList";
-import TitleSectionAnime from "@/components/TitleSectionAnime";
+import AnimeList from "@/components/AnimeList";
+import Header from "@/components/AnimeList/Header";
 
-export default async function Home(){
+export default async function Home() {
     const response = await fetch(`${process.env.NEXT_API_URL_PUBLIC_BASE}/top/anime?limit=8`)
-    const anime = await response.json();
+    const animeTop = await response.json();
+
     return (
         <div className="p-2">
-            <TitleSectionAnime title="Anime Populer" link="/" />
-            <div className="grid grid-cols-2 gap-4 mt-2 lg:grid-cols-4 md:grid-cols-3">
-                { anime.data.map((item) => {
-                    return (
-                        <div key={item.mal_id} className="shadow-xl">
-                            <CardAnimeList 
-                                id={item.mal_id} 
-                                title={item.title} 
-                                urlImg={item.images.jpg.image_url} 
-                                episode={item.episodes}
-                                score={item.score}   
-                                genres={item.genres} 
-                                />
-                        </div>
-                    ) 
-                }) }
-            </div>
+            <section>
+                <Header title="Anime Populer" link="/populer" />
+                <AnimeList api={animeTop} />
+            </section>
         </div>
     )
 }
--- src/components/AnimeList/Header.js
+++ src/components/AnimeList/Header.js
@@ -1,7 +1,8 @@
-import Link from "next/link"
+import Link from 'next/link';
+import React from 'react';
 
-const TitleSectionAnime = ({title,link}) => {
-    return (
+const Header = ({title,link}) => {
+     return (
         <div className="flex items-center justify-between my-2">
             <p className="text-xl font-bold">{title}</p>
             <Link className="text-sm font-semibold hover:text-indigo-600 sm:text-base hover:underline" href={link}>Lihat Semua</Link>
@@ -9,4 +10,4 @@ const TitleSectionAnime = ({title,link}) => {
     )
 }
 
-export default TitleSectionAnime
+export default Header;
