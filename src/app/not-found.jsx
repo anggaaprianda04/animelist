9c3c4fb0213a16763197e37de706da203c242bd4
anggaaprianda04 <anggaaprianda04@gmail.com>
2024-05-27 15:37:36 +0700

    refactor

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -7,8 +7,8 @@ const Page = () => {
   return (
     <div className="flex items-center justify-center max-w-xl min-h-screen mx-auto">
       <div className="flex flex-col items-center justify-center">
-        <FileSearch className="text-color-dark" size={64} />
-        <h3 className="text-2xl font-bold text-color-white">Not Found</h3>
+        <FileSearch className="text-color-accent" size={64} />
+        <h3 className="text-2xl font-bold text-color-white">Page Not Found</h3>
         <Link href="/">
           <button className="w-64 py-3 mt-2 text-xl font-semibold rounded-md hover:bg-opacity-90 hover:transition-all bg-color-accent text-color-dark px-7">
             HomePage
