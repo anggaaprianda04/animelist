7f71797a485bec4da47f43e3fc7ce7bdc4b6d4da
anggaaprianda04 <anggaaprianda04@gmail.com>
2024-05-28 22:31:59 +0700

    update

--- src/app/service/api-anime.js
+++ src/app/service/api-anime.js
@@ -1,7 +1,18 @@
 export const getAnimeResponse = async (resource, query) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`);
-    const anime = await response.json();
-    return anime;
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache, no-store, max-age=120, must-revalidate',
+        }
+    });
+    if (response.status === 200) {
+        const anime = await response.json();
+        return anime;
+    } else {
+        console.log(response.statusText);
+        return response.statusText;
+    }
+
 }
 
 export const getNestedAnimeResponse = async (resource, objectProperty) => {
