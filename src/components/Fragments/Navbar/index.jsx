7638d8bbf36dd378219b298c036bc98c1f492a4c
anggaaprianda04 <anggaaprianda04@gmail.com>
2024-06-02 01:30:24 +0700

    add

--- src/components/Fragments/Navbar/index.jsx
+++ src/components/Fragments/Navbar/index.jsx
@@ -3,7 +3,7 @@ import CardNavbar from "./CardNavbar";
 const Navbar = () => {
   return (
     <div className="flex justify-between bg-color-primary sm:items-center sm:p-4 sm:flex-row">
-      <div className="flex flex-col justify-start gap-2 mt-2 ml-4 sm:ml-0 sm:flex-row sm:mt-0 ">
+      <div className="flex flex-col justify-start gap-2 mt-2 mb-3 ml-4 sm:mb-0 sm:ml-0 sm:flex-row sm:mt-0 ">
         <CardNavbar link="/" title="Home" />
         <CardNavbar link="/manga" title="Manga" />
         <CardNavbar link="/characters" title="Characters" />
