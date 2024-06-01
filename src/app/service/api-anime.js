16b2bb49baaa94701cc2388147b0a8f5026ff980
anggaaprianda04 <anggaaprianda04@gmail.com>
2024-06-01 14:21:15 +0700

    update

--- src/app/service/api-anime.js
+++ src/app/service/api-anime.js
@@ -1,8 +1,5 @@
 export const getAnimeResponse = async (resource, query) => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`, {
-        next: {
-            revalidate: 3600
-        },
         cache: "force-cache"
     });
     return response.json();
